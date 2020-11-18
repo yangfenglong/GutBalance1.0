f0343410d89f0d3bfbc113b2a46191abb6a99813
yangfenglong <yangfenglong110@126.com>
2020-11-18 10:02:12 +0800

    delete reduncdant files

--- Data/amplicon/diseases_balance_species/changeid4arff.sh
+++
@@ -1,3 +0,0 @@
-for(`less otu_table.xls.balance.ids_old2new.csv`){chomp;@id=(split/,/);$hash{$id[0]}=$id[1];}; for(`less otu_table.xls.balance.arff`){if($_=~/attribute (.*) numeric/){print "\@attribute $hash{$1} numeric\n"}else{print $_} }' >otu_table.xls.balance.arff1
-ls D* -d | perl -ne 'chomp;for(`less $_/otu_table.xls.balance.ids_old2new.csv`){chomp;@id=(split/,/);$hash{$id[0]}=$id[1];}; open OUT, ">$_/otu_table.xls.balance.arff1"; for(`less $_/otu_table.xls.balance.arff`){if($_=~/attribute (.*) numeric/){print OUT "\@attribute $hash{$1} numeric\n"}else{print OUT $_} }'
-
--- Data/amplicon/diseases_balance_species/zip.sh
+++
@@ -1,2 +0,0 @@
-ls D* -d|grep -v D000067877|perl -ne 'chomp;$_=~s/\///;print "cd /mnt/d/database/GMrepo/amplicon/diseases_balance_species/$_; zip -m $_.zip otu_table.xls.balance.arff otu_table.xls.balance.libsvm otu_table.xls.balance.lastColAsLabels.csv\n"' |sh
-
